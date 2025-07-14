c302d3ae56b65dd615a37c7023bad7eaf6bfca9d
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2025-07-14 22:03:11 +0900

    Title: 사각형 채우기 3, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250714/사각형 채우기 3/rectangle-fill-3.java
+++ 250714/사각형 채우기 3/rectangle-fill-3.java
@@ -6,7 +6,7 @@ public class Main {
         // Please write your code here.
         //n=1 일때 2
         //n=2 일때 7 -> 2*2 + 3 
-        //n=3 22 -> 7*2 + 2*3 +   
+        //n=3 22 -> 7*2 + 2*3 +   //16/6
         //d[k] = d[k-1] * 7 + d[k-2] * 2 + d[k-3] * 2
         // d[k-1] * [1] -> 겹치는거 없음
         // d[k-2] * (d[2]) -> d[k-1] * [1]와 겹치는거 존재
@@ -20,9 +20,15 @@ public class Main {
             System.out.print(7);
             return;
         }
-        dp[0] =1; dp[1] = 2; dp[2] = 7;
-        for(int i=3;i<=n;i++) {
-            dp[i] = (dp[i-1]*2 + dp[i-2] * 3 + dp[i-3]*2) % 1000000007;
+        if(n==3) {
+            System.out.print(22);
+            return;
+        }
+        dp[1] = 2; dp[2] = 7; dp[3] = 22;
+        for(int i=4;i<=n;i++) {
+            dp[i] = (dp[i-1]*2 + dp[i-2]*3 + dp[i-3]*2) % 1000000007;
+            for(int j=n-i;j>=0;j--)
+                dp[i] = (dp[i] + 2) % 1000000007;
         }
 
         System.out.print(dp[n]);
