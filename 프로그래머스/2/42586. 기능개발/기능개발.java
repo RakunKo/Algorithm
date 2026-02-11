c08529398fd869eed3f0c779c66d8ed7e81a5e4f
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-11 20:32:03 +0900

    [level 2] Title: 기능개발, Time: 6.16 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,23 +1,29 @@
 import java.util.*;
+import java.lang.Math.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> list = new ArrayList<>();
         int size = speeds.length;
+        int[] remain = new int[size];
+        
+        for(int i=0;i<size;i++) {
+            remain[i] = (int) Math.ceil((double)(100 - progresses[i]) / speeds[i]);
+        }
         
         int idx = 0;
-        while(idx < size) {
-            for(int i=0;i<size;i++) progresses[i] += speeds[i];
+        while (idx < size) {
+            int cnt = 1;
+            int currentStandard = remain[idx]; 
+            int j = idx + 1;
 
-            if(progresses[idx] >= 100) {            
-                int cnt = 0;
-                for(int i=idx;i<size;i++) {
-                    if(progresses[i] < 100) break;
-                    cnt++;
-                }
-                list.add(cnt);
-                idx += cnt;   
+            while (j < size && remain[j] <= currentStandard) {
+                cnt++;
+                j++;
             }
+
+            list.add(cnt);
+            idx = j; 
         }
         
         return list.stream()
