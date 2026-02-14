3671b8c9cbb91564b1172b9b491a080f4ea8d45a
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-14 20:37:43 +0900

    [level 2] Title: 소수 찾기, Time: 80.16 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,38 @@
+class Solution {
+    int cnt = 0;
+    int[] vis = new int[10000000];
+    int[] visIdx = new int[7];
+    
+    public int solution(String numbers) {
+        dfs(0, numbers.toCharArray(), "0");
+        return cnt;
+    }
+    
+    public void dfs(int d, char[] chars, String number) {
+        if(d > chars.length) return;
+        
+        int n = Integer.parseInt(number);
+        System.out.println(n);
+        if(vis[n] == 1) return;
+        if(isPrime(n)) cnt++;
+    
+        vis[n] = 1;
+        for(int i=0;i<chars.length;i++) {
+            if(visIdx[i] == 1) continue;
+            int num = chars[i] - '0';
+            visIdx[i] = 1;
+            dfs(d+1, chars, number+num);
+            visIdx[i] = 0;
+        }
+    }
+    
+    public boolean isPrime(int num) {
+        if(num == 1 || num == 0) return false;
+        if(num == 2) return true;
+        
+        for(int i=2;i<num/2+1;i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
