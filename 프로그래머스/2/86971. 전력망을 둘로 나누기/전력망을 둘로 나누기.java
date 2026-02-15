7280cf8f41164586e1bcf5fa236e6a2531877096
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-15 21:31:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.13 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,28 +4,26 @@ import java.lang.Math.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         // n-1개의 간선은 한개 끊으면 무조건 2개로 나눠짐
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        // n개의 vertex와 n-1개의 간선일 경우 무조건 트리
+        List<Integer>[] adj = new ArrayList[n+1];
+        
+        for (int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
         
         for(int[] wire: wires) {
-            Set<Integer> s1 = graph.getOrDefault(wire[0], new HashSet<>());
-            s1.add(wire[1]);
-            graph.put(wire[0], s1);
-            
-            Set<Integer> s2 = graph.getOrDefault(wire[1], new HashSet<>());
-            s2.add(wire[0]);
-            graph.put(wire[1], s2);
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
         }
         
         int answer = 101;
         for(int[] wire: wires) {
-            int cnt = explore(graph, wire[0], wire[1], n);
+            int cnt = explore(adj, wire[0], wire[1], n);
             answer = Math.min(answer, Math.abs(cnt - Math.abs(cnt-n)));
         }
         
         return answer;
     }
     
-    public int explore(Map<Integer, Set<Integer>> graph, int v1, int v2, int n) {
+    public int explore(List<Integer>[] adj, int v1, int v2, int n) {
         Deque<Integer> dq = new ArrayDeque<>();
         boolean[] vis = new boolean[n+1];
         dq.offer(v1);
@@ -35,9 +33,8 @@ class Solution {
         while(!dq.isEmpty()) {
             cnt++;
             int vertex = dq.poll();
-            
-            Set<Integer> set = graph.get(vertex);
-            for(Integer i: set) {
+
+            for(Integer i: adj[vertex]) {
                 if(vertex == v1 && i == v2) continue;
                 if(vertex == v2 && i == v1) continue;
                 if(vis[i]) continue;
