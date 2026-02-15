ad9ff62f1f43722e61a2524ed7d6e9a773a87bfa
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-15 21:46:56 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,5 @@
+select ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPERS
+    where (
+        SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE NAME IN ('Python', 'C#'))
+    ) != 0
+    order by ID;
