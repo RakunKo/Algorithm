480bbae50e5aa1240c621318528eaa935faa66ab
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-21 16:30:30 +0900

    [level 2] Title: 지게차와 크레인, Time: 35.73 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class Solution {
+    char[][] containers;
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    
+    class Pos {
+        int x; int y;
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        int h = storage.length;
+        int w = storage[0].length();
+        int totalSize = h*w;
+        
+        containers = new char[h+2][w+2];
+        
+        for(int i=1;i<h+1;i++) {
+            char[] arr = storage[i-1].toCharArray();
+            for(int j=1;j<w+1;j++) containers[i][j] = arr[j-1];
+        }
+        
+        for(String request: requests) {
+            boolean type = false;
+            if(request.length() > 1) type = true;
+            
+            answer += release(request.charAt(0), type, h, w);
+        }
+        
+        
+        return totalSize - answer;
+    }
+    
+    public int release(char c, boolean type, int h, int w) {
+        int cnt = 0;
+        if(type) {
+            for(int i=1;i<=h;i++) {
+                for(int j=1;j<=w;j++) {
+                    if(containers[i][j] == c) {
+                        containers[i][j] = ' ';
+                        cnt++;
+                    }
+                }
+            }
+        }
+        else {
+            boolean[][] outside =  new boolean[h+2][w+2];
+            Queue<Pos> q = new LinkedList<>();
+            q.offer(new Pos(0,0));
+            outside[0][0] = true;
+            
+            while(!q.isEmpty()) {
+                Pos p = q.poll();
+                
+                for(int i=0;i<4;i++) {
+                    int dx = p.x + dir[i][0];
+                    int dy = p.y + dir[i][1];
+                    
+                    if(dx < 0 || dx >= h+2 || dy < 0 || dy >= w+2) continue;
+                    if(outside[dx][dy]) continue;
+                    if(containers[dx][dy] >= 'A' && containers[dx][dy] <= 'Z') continue;
+                    
+                    q.offer(new Pos(dx, dy));
+                    outside[dx][dy] = true;
+                }
+            }
+            
+            for(int i=0;i<=h+1;i++) {
+                for(int j=0;j<=w+1;j++) {
+                    if(outside[i][j]) {
+                        for(int k=0;k<4;k++) {
+                            int di = i + dir[k][0];
+                            int dj = j + dir[k][1];
+                            
+                            if(di < 0 || di >= h+2 || dj < 0 || dj >= w+2) continue;
+                            if(outside[di][dj]) continue;
+                            if(containers[di][dj] != c) continue;
+                            
+                            containers[di][dj] = ' ';
+                            cnt++;   
+                        }
+                    }
+                }
+            }
+        }
+        
+        return cnt;
+    }
+}
