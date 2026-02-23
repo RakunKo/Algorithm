2f01f162fa794cd07cb4817bb6ffe5d770e3655f
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-23 23:39:12 +0900

    [level 1] Title: 신고 결과 받기, Time: 228.55 ms, Memory: 188 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        Map<String, Integer> status = new HashMap<>();
+        Map<String, Set<String>> users = new HashMap<>();
+        Map<String, Integer> idx = new HashMap<>();
+        
+        for(int i=0;i<id_list.length;i++) idx.put(id_list[i], i);
+        
+        for(String r: report) {
+            String[] user = r.split(" ");
+            if(!users.getOrDefault(user[0], new HashSet<>()).contains(user[1])) {
+                status.put(user[1], status.getOrDefault(user[1], 0) + 1);
+            }
+            
+            Set<String> set = users.getOrDefault(user[0], new HashSet<>());
+            set.add(user[1]);
+            users.put(user[0], set);
+        }
+        
+        Set<String> ban = new HashSet<>();
+        for(Map.Entry<String, Integer> e: status.entrySet()) {
+            if(e.getValue() >= k) ban.add(e.getKey());
+        }
+        
+        for(Map.Entry<String, Set<String>> e: users.entrySet()) {
+            String user = e.getKey();
+            for(String u: e.getValue()) {
+                if(ban.contains(u)) answer[idx.get(user)]++;
+            }
+        }
+        
+        return answer;
+    }
+}
