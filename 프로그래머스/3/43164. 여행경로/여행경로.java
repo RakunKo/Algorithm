97138c03de976b3ff03e37d40f1dff66139cb16a
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-02-28 15:36:13 +0900

    [level 3] Title: 여행경로, Time: 27.32 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    boolean[] vis;
+    List<String> answer = new ArrayList<>();
+     
+    public String[] solution(String[][] tickets) {
+        int n = tickets.length;
+        
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        vis = new boolean[n+1];
+        dfs(0, "ICN", tickets, n, new ArrayList<>());
+        
+        return answer.stream().toArray(String[]::new);
+    }
+    
+    public void dfs(int d, String airport, String[][] tickets, int n, List<String> ap) {
+        if(d == n && answer.size() == 0) {
+            ap.add(airport);
+            answer = new ArrayList<>(ap);
+            return;
+        }
+        
+        for(int i=0;i<n;i++) {
+            // String 비교는 equals 사용(객체임)
+            if(vis[i] || !tickets[i][0].equals(airport)) continue;
+            vis[i] = true;
+            ap.add(tickets[i][0]);
+            dfs(d+1, tickets[i][1], tickets, n, ap);
+            ap.remove(ap.size() - 1);
+            vis[i] = false;
+        }
+    }
+}
