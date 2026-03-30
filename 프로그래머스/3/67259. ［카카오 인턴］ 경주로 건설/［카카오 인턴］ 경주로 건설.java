5ddd2836e1d93e248aded9ab37f2c724d575bec5
HyunCheolKo <145656942+RakunKo@users.noreply.github.com>
2026-03-30 22:24:49 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 21.71 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,49 +1,60 @@
 import java.lang.Math.*;
+import java.util.*;
 
 class Solution {
-    int answer = Integer.MAX_VALUE;
+    class Pos {
+        int x; int y; int cost; int dir;
+        public Pos(int x, int y, int cost, int dir) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+            this.dir = dir;
+        }
+    }
     
     int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
     int[][][] vis;
-    int n;
     
     public int solution(int[][] board) {
-        n = board.length;
+        int n = board.length;
         vis = new int[n][n][4];
         
         for(int i=0;i<n;i++) 
             for(int j=0;j<n;j++) 
                 for(int k=0;k<4;k++) vis[i][j][k] = Integer.MAX_VALUE;
         
-        for(int i=0;i<4;i++) vis[0][0][i] = 0;
-        dfs(board, 0, 0, 0, 0);
-        
-        return answer;
+        return dijkstra(board, n);
     }
     
-    public void dfs(int[][] board, int x, int y, int prev, int cost) {
-        //System.out.println(x + " " + y);
-        if(answer <= cost) return;
+    public int dijkstra(int[][] board, int n) {
+        PriorityQueue<Pos> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);   
         
-        if(x == n-1 && y == n-1) {
-            answer = Math.min(answer, cost);
-            return;
-        }
+        for(int i=0;i<4;i++) vis[0][0][i] = 0;
+        pq.offer(new Pos(0,0,0,0));
         
-        for(int i=0;i<4;i++) {
-            int dx = x + dir[i][0];
-            int dy = y + dir[i][1];
+        while(!pq.isEmpty()) {
+            Pos p = pq.poll();
             
-            if(dx >= n || dx < 0 || dy >= n || dy < 0) continue;
-            if(board[dx][dy] == 1) continue;
+            if(vis[p.x][p.y][p.dir] < p.cost) continue;
+            if(p.x == n-1 && p.y == n-1) return p.cost;
             
-            int curr = 100;
-            if(!(x == 0 && y == 0) && prev != i) curr += 500;
-            
-            if(vis[dx][dy][i] < cost + curr) continue;
-            
-            vis[dx][dy][i] = cost + curr;
-            dfs(board, dx, dy, i, cost + curr);
+            for(int i=0;i<4;i++) {
+                int dx = p.x + dir[i][0];
+                int dy = p.y + dir[i][1];
+                
+                if(dx >= n || dx < 0 || dy >= n || dy < 0) continue;
+                if(board[dx][dy] == 1) continue;
+                
+                int curr = 100;
+                if(!(p.x == 0 && p.y == 0) && p.dir != i) curr += 500;
+                
+                if(vis[dx][dy][i] < p.cost + curr) continue;
+                
+                vis[dx][dy][i] = p.cost + curr;
+                pq.offer(new Pos(dx, dy, p.cost + curr, i));
+            }
         }
+    
+        return 0;
     }
 }
